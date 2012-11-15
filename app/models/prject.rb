2b1551993a92292dea228e1df75463c879314c61
David House <davidtwhouse@gmail.com>
2012-11-15 22:36:27 +0000

    Create of projects feature complete

---
+++ app/models/prject.rb
@@ -0,0 +1,3 @@
+class Prject < ActiveRecord::Base
+  attr_accessible :name
+end
---
+++ spec/models/prject_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Prject do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
