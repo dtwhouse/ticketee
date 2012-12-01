ac4df4d5605f933d943ae27d71092827d06aefe0
David House <davidtwhouse@gmail.com>
2012-12-01 23:12:09 +0000

    Added admin namespeces users controller

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,8 +1,18 @@
 Given /^there are the following users:$/ do |table|
   table.hashes.each do |attributes|
+    #unconfirmed = attributes.delete("unconfirmed") == "true"
+    #@user = User.create!(attributes)
+    #@user.update_attribute("admin", attributes["admin"] == "true")
+    #@user.confirm! unless unconfirmed
+
     unconfirmed = attributes.delete("unconfirmed") == "true"
-    @user = User.create!(attributes)
-    @user.confirm! unless unconfirmed
+    isadmin = attributes.delete("admin") == "true"
+    @user = User.new(attributes)
+    @user.admin = "true" if isadmin
+    @user.save
+    @user.confirm! unless unconfirmed 
+
+
   end
 end
 
